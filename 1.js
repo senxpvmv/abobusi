df8d000bc3e65f7eb87e13867980e7c4f844f961
txssdghxst <kuprinenkok58@gmail.com>
2021-10-19 01:08:25 +0300

    inputs

--- 1.js
+++ 1.js
@@ -1,10 +1,28 @@
+let test = ["Київ","Житомир","Новоград","Рівне","Броди","Львів"]
+
+
 let trips = [
-	KyivLviv = {duration: "5 годин", stop1: "Житомир", stop2: "Новоград", stop3: "Рівне", stop4: "Броди",},
-	LvivOdessa = {duration: "7 годин", stop1: "Умань", stop2: "Вінниця", stop3: "Хмельницький", stop4: "Тернопіль",},
-	KharkivMykolaiv = {duration: "10 годин", stop1: "Каховка", stop2: "Мелітополь", stop3: "Волноваха", stop4: "Бердянск",}
+	KyivLviv = {duration: "5 годин", stop1: "Київ", stop2: "Житомир", stop3: "Новоград", stop4: "Рівне", stop5: "Броди", stop6: "Львів"},
+	LvivOdessa = {duration: "7 годин", stop1: "Львів", stop2: "Умань", stop3: "Вінниця", stop4: "Хмельницький", stop5: "Тернопіль", stop6: "Одесса"},
+	KharkivMykolaiv = {duration: "10 годин", stop1: "Харків", stop2: "Каховка", stop3: "Мелітополь", stop4: "Волноваха", stop5: "Бердянск", stop6: "Миколаїв"}
 ]
 
 let buses = [
 	bus1 = {name: "Еталон", enginetype: "Дизельний", seating: "28", horsepower: "130", info: "Туристичний автобус малого класу. Призначений для використання на маршрутах середньої дальності. Володіє підвищеною пасажиромісткістю. В салоні – припіднята підлога, шторки, багажні полички, система гучного зв′язку.",},
 	bus2 = {name: "Sprinter", enginetype: "Дизельний", seating: "28", horsepower: "130", info: "" },
 ]
+
+let qw = form.elements.to
+aler(qw)
+
+// alert(trips.includes(to))
+
+// for(let key in trips){
+// 	key[] === to
+// 	alert(key)
+// }
+
+// alert(trips[2].duration)
+// for (var i=0 ; i<trips[2].length; i++){
+// 	document.write(trips[2])
+// }
